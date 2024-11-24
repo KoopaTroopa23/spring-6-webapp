308dde1963655b3112dee64d20b36921b803088b
Kevin <57780570+KoopaTroopa23@users.noreply.github.com>
2024-11-24 13:04:36 -0500

    Added Swagger integration to BookController and updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.3.5"
     id("io.spring.dependency-management") version "1.1.6"
-    id("io.freefair.lombok") version "8.0.1" // Lombok plugin for automatic configuration
+    id("io.freefair.lombok") version "8.0.1"
+    id("org.springframework.boot") version "3.3.6" // Lombok plugin for automatic configuration
 }
 
 group = "guru.springframework"
@@ -30,6 +30,7 @@ dependencies {
     runtimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
 }
 
 dependencyManagement {
--- src/main/java/guru/springframework/spring6webapp/controllers/AuthorController.java
+++ src/main/java/guru/springframework/spring6webapp/controllers/AuthorController.java
@@ -1,11 +1,19 @@
 package guru.springframework.spring6webapp.controllers;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import guru.springframework.spring6webapp.services.AuthorService;
+import guru.springframework.spring6webapp.domain.Author;
 
-@Controller
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/authors")
+@Tag(name = "Author Controller", description = "API for managing authors")
 public class AuthorController {
 
     private final AuthorService authorService;
@@ -14,9 +22,9 @@ public class AuthorController {
         this.authorService = authorService;
     }
 
-    @RequestMapping("/authors")
-    public String getAuthors(Model model) {
-        model.addAttribute("authors", authorService.findAll());
-        return "authors";
+    @GetMapping
+    @Operation(summary = "Get all authors", description = "Returns a list of all authors")
+    public List<Author> getAuthors() {
+        return (List<Author>) authorService.findAll();
     }
 }
